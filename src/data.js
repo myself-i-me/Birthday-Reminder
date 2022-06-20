29d3626d1def7a6583bb1a8ad6c4b54a4a2e10f6
myself-i-me <ganeshdarode121@gmail.com>
2022-06-20 08:44:15 +0530

    celebs updated

--- src/data.js
+++ src/data.js
@@ -1,37 +1,37 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
+    name: 'Nithin',
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      'https://scontent.fhyd8-1.fna.fbcdn.net/v/t1.6435-9/202231817_362217221929781_6555435831428274258_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=ckSRGfwzT-MAX_MxNKI&_nc_ht=scontent.fhyd8-1.fna&oh=00_AT9qTNA3B43BbAjhaZ3DL3o1YFI-jFUPPPDupK_D8I9nJg&oe=62D49D6E',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
+    name: 'Kajal Agrawal',
     age: 32,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      'https://scontent.fhyd8-1.fna.fbcdn.net/v/t1.6435-9/50282476_752918648407545_8018855373588922368_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=LFnMEwl6y3cAX_SXgV4&_nc_ht=scontent.fhyd8-1.fna&oh=00_AT-h2Q_AUWGv0JDl6ZuM0-LwbbfGVeqb1Hug00qsBloJcQ&oe=62D499F1',
   },
   {
     id: 3,
-    name: 'Larry Little',
-    age: 36,
+    name: 'Nagarajuna',
+    age: 62,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      'https://imgnew.outlookindia.com/uploadimage/library/16_9/16_9_5/IMAGE_1646925904.webp',
   },
   {
     id: 4,
-    name: 'Sean Walsh',
-    age: 34,
+    name: 'Nandamuri Balakrishna',
+    age: 62,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      'https://imgnew.outlookindia.com/public/uploads/articles/2021/11/8/Akhanda_3_adobespark.jpg',
   },
   {
     id: 5,
-    name: 'Lola Gardner',
+    name: 'Tamannaah Bhatia',
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://filmfare.wwmindia.com/content/2022/jun/tamannaahbhatia11654078196.jpg',
   },
 ];
